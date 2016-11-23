d29f0bb33df5528d2ea779a6fc73f6f01d39e111
jelly074100209 <419756656@qq.com>
2016-11-23 10:40:16 +0800

    JSS

--- src/com/pattern/adapter/Target.java
+++ src/com/pattern/adapter/Target.java
@@ -4,6 +4,6 @@ package com.pattern.adapter;
  * Created by JSS on 2016/11/23.
  */
 public interface Target {
-    public void Request1();
-    public void Request2();
+    void Request1();
+    void Request2();
 }
--- src/com/pattern/factory_method/Pen.java
+++ src/com/pattern/factory_method/Pen.java
@@ -4,5 +4,5 @@ package com.pattern.factory_method;
  * Created by JSS on 2016/11/23.
  */
 public interface Pen {
-    public void display();
+    void display();
 }
